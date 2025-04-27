6c198f1e590d46855fe606ddc1fd2dbabbe35110
eliasmr98 <eliasmartinrivas@gmail.com>
2025-04-27 18:53:27 +0200

    feat: enhance Projects and SectionContainer components for improved styling and responsiveness; add Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx}'
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
